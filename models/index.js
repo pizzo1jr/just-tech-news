0433423ed69ed880eda152517ba6bffcd097adad
pizzo1jr <102200085+pizzo1jr@users.noreply.github.com>
2023-01-28 18:31:32 -0500

    votes added

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require('./User');
 const Post = require('./Post');
+const Vote = require('./Vote');
 
 // create associations User can post as many posts as wanted 
 User.hasMany(Post, {
@@ -11,5 +12,33 @@ Post.belongsTo (User, {
     foreignKey: 'user_id',
 });
 
-module.exports = { User, Post };
+User.belongsToMany(Post, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'user_id'
+});
+
+Post.belongsToMany(User, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'post_id'
+});
+
+Vote.belongsTo(User, {
+    foreignKey:'user_id'
+});
+
+Vote.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+User.hasMany(Vote, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Vote, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote };
 
