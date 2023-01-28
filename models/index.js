e042762ce9291f25b380e69fd9adc945356baee2
pizzo1jr <102200085+pizzo1jr@users.noreply.github.com>
2023-01-28 16:12:18 -0500

    post updates

--- models/index.js
+++ models/index.js
@@ -1,5 +1,15 @@
 const User = require('./User');
 const Post = require('./Post');
 
+// create associations User can post as many posts as wanted 
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+// Many posts can only belong to one user
+Post.belongsTo (User, {
+    foreignKey: 'user_id',
+});
+
 module.exports = { User, Post };
 
